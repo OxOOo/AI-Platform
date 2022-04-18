4322de344697da6f1e31a633dea98f9af84a4107
chenyu <believe.chenyu@gmail.com>
2022-04-18 23:21:21 +0800

    update README.md

---
+++ code/random.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+const int MAXN = 11;
+int player;
+int chessboard[MAXN][MAXN];
+
+int main()
+{
+    // 如果有什么初始化，可以写在这里
+    // 初始化结束输出`init end`
+    cout << "init end" << endl;
+
+    while (cin >> player)
+    {
+        for (int i = 0; i < MAXN; i++)
+        {
+            for (int j = 0; j < MAXN; j++)
+            {
+                cin >> chessboard[i][j];
+            }
+        }
+
+        // 随机选择一个没有棋子的位置输出
+        int x, y;
+        while (true)
+        {
+            x = rand() % MAXN;
+            y = rand() % MAXN;
+            if (chessboard[x][y] == 0)
+                break;
+        }
+        cout << x << " " << y << endl;
+    }
+
+    return 0;
+}
